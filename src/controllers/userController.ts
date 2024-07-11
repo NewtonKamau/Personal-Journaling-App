391cb258a2037098ffc628988c24462fb5961c41
NewtonKamau <kamaunewton78@gmail.com>
2024-07-11 09:39:48 +0300

    getUser,updateUserDetails and removeUser methods

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,10 +1,14 @@
 import { Request, Response } from "express";
-import jwt from "jsonwebtoken";
-import { createUser, findUserByUsername } from "../models/Users";
+import {
+  createUser,
+  findUserByUsername,
+  updateUser,
+  deleteUser,
+} from "../models/Users";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const register = async (req: Request, res: Response) => {
- 
   try {
     const user = await createUser(req.body);
     res.status(201).json(user);
@@ -30,5 +34,63 @@ export const login = async (req: Request, res: Response) => {
     process.env.JWT_SECRET as string,
     { expiresIn: "1h" }
   );
-  res.json({ userName: user.username, token, success: "You are logged in." });
+  res.json({ token });
+};
+
+export const getUser = async (req: Request, res: Response) => {
+  try {
+    const user = await findUserByUsername(req.params.username);
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    res.json(user);
+  } catch (err) {
+    if (err instanceof Error) {
+      res.status(500).json({ error: err.message });
+    } else {
+      res.status(500).json({ error: "Unknown error occurred" });
+    }
+  }
+};
+
+export const updateUserDetails = async (req: Request, res: Response) => {
+  try {
+    if (!req.user) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+    const updatedUser = await updateUser(req.user.userId, req.body);
+    if (!updatedUser) {
+      return res
+        .status(404)
+        .json({ error: "User not found or not authorized to update" });
+    }
+    res.json(updatedUser);
+  } catch (err) {
+    if (err instanceof Error) {
+      res.status(500).json({ error: err.message });
+    } else {
+      res.status(500).json({ error: "Unknown error occurred" });
+    }
+  }
+};
+
+export const removeUser = async (req: Request, res: Response) => {
+  try {
+    if (!req.user) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+    const deleted = await deleteUser(req.user.userId);
+    if (!deleted) {
+      return res
+        .status(404)
+        .json({ error: "User not found or not authorized to delete" });
+    }
+    res.json({ message: "User deleted successfully" });
+  } catch (err) {
+    if (err instanceof Error) {
+      res.status(500).json({ error: err.message });
+    } else {
+      res.status(500).json({ error: "Unknown error occurred" });
+    }
+  }
 };
