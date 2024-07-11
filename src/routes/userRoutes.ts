b3c8f3d7dc27a221b46a2eeafdf981b8673363d6
NewtonKamau <kamaunewton78@gmail.com>
2024-07-11 09:35:49 +0300

    find user by name, update and delete user routes

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,9 +1,18 @@
 import { Router } from "express";
-import { register, login } from "../controllers/userController";
+import {
+  register,
+  login,
+  getUser,
+  updateUserDetails,
+  removeUser,
+} from "../controllers/userController";
+import { authenticateJWT } from "../middleware/auth";
 
 const router = Router();
 
 router.post("/register", register);
 router.post("/login", login);
-
+router.get("/:username", authenticateJWT, getUser);
+router.put("/", authenticateJWT, updateUserDetails);
+router.delete("/", authenticateJWT, removeUser);
 export default router;
